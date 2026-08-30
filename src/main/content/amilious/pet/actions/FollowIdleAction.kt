9dfc1ccaaaca1a4950bccd87f006c76868fb28a8
Brandon Anderson <brandon.anderson@amilious.com>
2026-08-30 06:05:35 -0500

    monkey test 63

--- src/main/content/amilious/pet/actions/FollowIdleAction.kt
+++ src/main/content/amilious/pet/actions/FollowIdleAction.kt
@@ -1,12 +1,20 @@
 package content.amilious.pet.actions
 
 import content.amilious.pet.AmiliousMonkey
+import content.amilious.pet.MonkeyConfig
 
 class FollowIdleAction : CompanionAction<AmiliousMonkey> {
     override fun name() = "follow"
-    override fun canStart(actor: AmiliousMonkey) = !actor.pulseManager.hasPulseRunning()
+
+    override fun canStart(actor: AmiliousMonkey) =
+        actor.location.getDistance(actor.owner.location) > 1.5 &&
+                actor.location.getDistance(actor.owner.location) <= MonkeyConfig.FOLLOW_DIST
+
     override fun tick(actor: AmiliousMonkey): Boolean {
-        actor.followOwner()
-        return false
+        val dist = actor.location.getDistance(actor.owner.location)
+        if (dist > MonkeyConfig.FOLLOW_DIST) return false
+        if (dist <= 1.5) return false
+        if (!actor.pulseManager.hasPulseRunning()) actor.followOwner()
+        return true
     }
 }
